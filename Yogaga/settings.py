b49a706e2077397d0ce30fee9c20d6696e7dcf16
utopianist <528645012@qq.com>
2019-05-20 23:54:21 +0800

    Improve crawl power.

--- Yogaga/settings.py
+++ Yogaga/settings.py
@@ -29,7 +29,8 @@ ROBOTSTXT_OBEY = False
 # See also autothrottle settings and docs
 #DOWNLOAD_DELAY = 3
 # The download delay setting will honor only one of:
-#CONCURRENT_REQUESTS_PER_DOMAIN = 16
+CONCURRENT_ITEMS = 200
+CONCURRENT_REQUESTS_PER_DOMAIN = 30
 #CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
